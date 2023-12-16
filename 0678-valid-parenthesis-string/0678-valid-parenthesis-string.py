40f32ffba391bef7662fa490418e807f54795114
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2023-12-16 17:37:47 -0500

    Time: 39 ms (52.78%), Space: 16.2 MB (60.55%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        # Time Complexity: O(n)
+        leftMin, leftMax = 0, 0
+
+        for c in s:
+            if c == "(":
+                leftMin, leftMax = leftMin + 1, leftMax + 1 
+            elif c == ")":
+                leftMin, leftMax = leftMin - 1, leftMax - 1
+            else:
+                # this is in case of the wildcard 
+                leftMin, leftMax = leftMin - 1 , leftMax + 1 
+            if leftMax < 0:
+                return False 
+            if leftMin < 0: # s = ( * ) (
+                leftMin = 0 
+        return leftMin == 0 
