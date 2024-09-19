d84dd948752052eaca41acb856afe6745f59ebcc
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-19 08:02:42 -0400

    Time: 352 ms (65.28%), Space: 17.7 MB (67.01%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -3,11 +3,11 @@ class Solution:
         # Time Complexity: O(n)
         # we will work our way backwards 
         goal = len(nums) - 1
-
+   
         # we travel backwards 
-        for i in range(len(nums)-1,-1,-1):
+        for i in range(len(nums)-1, -1, -1):
             if i + nums[i] >= goal:
-                # this means we can reach the goal 
                 goal = i 
         
-        return True if goal==0 else False 
+        return True if goal == 0 else False 
+ 
