5c899a4e9b615035f512301180906e4a91bde33c
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-19 09:31:39 -0400

    Time: 164 ms (52.75%), Space: 18.4 MB (54.34%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,29 +1,25 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        # Time Complexity: O(n*logn)
+        # Time Complexity: O(n * logn)
+        # If there is a remainder then we return False 
         if len(hand) % groupSize:
             return False 
         
         count = {}
         for n in hand:
-            count[n] = 1 + count.get(n,0)
-
-        # we want minHeap with distinct values that are available to us 
+            count[n] = 1 + count.get(n, 0)
+        
         minH = list(count.keys())
         heapq.heapify(minH)
-
         while minH:
             first = minH[0]
 
-            for i in range(first, first+groupSize):
-                if i not in count:
+            for val in range(first, first + groupSize):
+                if val not in count:
                     return False 
-                count[i] -= 1 
-                if count[i] == 0:
-                    # if the value we are popping is not the min val then we will return false immediately 
-                    if i != minH[0]:
+                count[val] -= 1
+                if count[val] == 0:
+                    if val != minH[0]:
                         return False 
                     heapq.heappop(minH)
         return True 
-
-
