625c3343e86896862e17c74b1c46e5720a06421c
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-11-06 14:56:15 -0500

    Time: 7 ms (97.70%), Space: 17.3 MB (95.97%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,13 +1,9 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # Time Complexity: O(n)
-        # we will work our way backwards 
         goal = len(nums) - 1
-   
-        # we travel backwards 
+        
         for i in range(len(nums)-1, -1, -1):
             if i + nums[i] >= goal:
                 goal = i 
-        
+
         return True if goal == 0 else False 
- 
